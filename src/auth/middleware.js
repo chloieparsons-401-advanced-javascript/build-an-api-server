64779bc07ae31e2e105c5286301c2bfa3b733dde
Chloie Parsons <chloieparsons@gmail.com>
2019-06-16 17:20:19 -0700

    Began modularization of middleware file. AuthBasic completed.

--- src/auth/middleware.js
+++ src/auth/middleware.js
@@ -22,24 +22,12 @@ module.exports = (capability) => {
     }
 
 
-    function _authBasic(str) {
-    // str: am9objpqb2hubnk=
-      let base64Buffer = Buffer.from(str, 'base64'); // <Buffer 01 02 ...>
-      let bufferString = base64Buffer.toString();    // john:mysecret
-      let [username, password] = bufferString.split(':'); // john='john'; mysecret='mysecret']
-      let auth = {username, password}; // { username:'john', password:'mysecret' }
-
-      return User.authenticateBasic(auth)
-        .then(user => _authenticate(user))
-        .catch(_authError);
-    }
-
     function _authBearer(authString) {
       return User.authenticateToken(authString)
         .then(user => _authenticate(user))
         .catch(_authError);
     }
-
+  
     function _authenticate(user) {
       if ( user && (!capability || (user.can(capability))) ) {
         req.user = user;
